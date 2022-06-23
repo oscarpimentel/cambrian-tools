77e1fb9bb2c582822aff118c0ffcaa96fb38f6df
opimentel-tesla <oscarlo.pimentel@gmail.com>
2022-06-23 18:13:32 -0400

    init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 source ~/.bashrc
+repo_name=${PWD##*/}
+env_name=$repo_name
 git init
 rm .gitignore
 echo '*.secrets*' >> .gitignore
@@ -7,13 +9,17 @@ echo '*.pyc*' >> .gitignore
 echo '*.ipynb_checkpoints*' >> .gitignore
 echo '*__pycache__*' >> .gitignore
 echo '.vscode/' >> .gitignore
+echo '.egg-info' >> .gitignore
 echo 'temp/' >> .gitignore
 echo 'save/' >> .gitignore
 echo 'data/' >> .gitignore
 echo 'debug/' >> .gitignore
 echo 'dist/' >> .gitignore
-repo_name=${PWD##*/}
-env_name=$repo_name
 conda deactivate
 conda create -n $env_name python=3.10 -y
 conda activate $env_name
+conda install -c conda-forge nbstripout -y
+nbstripout --install
+nbstripout --status
+echo "========================================================================="
+echo "enter the new environment by using: conda activate $env_name"
